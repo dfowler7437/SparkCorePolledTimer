4e7336cd207be4e27fe2e3c13fa62215c1e02569
dfowler7437 <david.fowler@gmai.com>
2014-11-16 18:11:28 -0600

    Still doing the inital commit. Better next time.

---
+++ PolledTimerExample.ino
@@ -0,0 +1,17 @@
+#include "PolledTimer.h"
+
+PolledTimer updateTimer(1000);  //Create a timer object and set it's timeout in milliseconds
+void OnTimer(void);   //Prototype for timer callback method
+
+void setup() {
+  Serial1.begin(115200);  
+  updateTimer.SetCallback(OnTimer);
+}
+
+void loop() {
+   updateTimer.Update();
+}
+
+void OnTimer(void) {  //Handler for the timer, will be called automatically
+  Serial1.println("Hello");
+}
